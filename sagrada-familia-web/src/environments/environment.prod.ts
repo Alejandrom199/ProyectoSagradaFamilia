b73828c36060e662f3aac447cbd32bdbd322b707
Alejandro <alejandrom199916@gmail.com>
2026-06-17 00:35:39 -0500

    fix(frontend): actualizar URL de la API a Railway

--- sagrada-familia-web/src/environments/environment.prod.ts
+++ sagrada-familia-web/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://sagrada-familia-api.onrender.com/api'
+    apiUrl: 'https://proyectosagradafamilia-production.up.railway.app/api'
 };
